1c704758c4c60586774ffbbd541598a3a68d3b27
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-04-21 18:44:51 +0200

    debugged details

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,26 +1,27 @@
-import './detail.css'
+import React from "react";
+import "./detail.css";
 
-interface detailsProps{
-    title: string;
-    description?: string;
-    items?: string[];
+interface DetailsProps {
+  title: string;
+  description?: string;
+  items?: string[];
 }
 
-function Details({title,description,items}: detailsProps){
-    if(!items){
-        return;
-    }
-    return (
-        <div className="details-container">
-            <h2 className='ttt'>{title}</h2>
-            <p className='ddd'>{description}</p>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    );
+function Details({ title, description, items }: DetailsProps) {
+  if (!items || items.length === 0) {
+    return null;
+  }
+  return (
+    <div className="details-container">
+      <h2 className="ttt">{title}</h2>
+      <p className="ddd">{description}</p>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Details;
